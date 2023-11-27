12d77a79dc166354003fdc488578fe317fa7bfec
Sardor Irgashev <irgashevsardor99@gmail.com>
2023-11-27 23:07:51 +0500

    Modify index.js

--- 2.Containers_VMs/2_4/hello_docker/index.js
+++ 2.Containers_VMs/2_4/hello_docker/index.js
@@ -1,11 +1,10 @@
 const express = require('express')
 const app = express()
-const port = 3000
 
 app.get('/', (req, res) => {
   res.send('Hello, Docker!')
 })
 
 app.listen(port, () => {
-  console.log(`App listening on port ${port}`)
+  console.log(`App is running!}`)
 })
